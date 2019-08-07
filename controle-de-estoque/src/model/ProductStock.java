4f89885f81e3d15c8153722358eab21630c3c827
Lucas S <lucas.soaresouza@gmail.com>
2019-08-07 13:30:10 -0300

    Creating the add methods on productstock

--- controle-de-estoque/src/model/ProductStock.java
+++ controle-de-estoque/src/model/ProductStock.java
@@ -18,19 +18,17 @@ public class ProductStock {
     private ArrayList<User> users;
     
     private ProductStock(){
-        this.products = null;
-        this.users = null;
+        this.products =  new ArrayList<>();
+        this.users =  new ArrayList<>();
     }
     
     public static ProductStock getStock(){
-        
         if(stock == null){
             stock = new ProductStock();
             return stock;
         } else {
             return stock;
         }
-        
     }
     
     public ArrayList<Product> getProducts(){
@@ -41,4 +39,43 @@ public class ProductStock {
         return this.users;
     }
     
+    public boolean addProduct(Product product){
+        boolean productExists;        
+        productExists = checkProductExistence(product);
+        if(!productExists){
+            return products.add(product);
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean addUser(User user){
+        boolean userExists;
+        userExists = checkUserExistence(user);
+        if(!userExists){
+            return users.add(user);
+        } else {
+            return false;
+        }        
+    }
+    
+    private boolean checkProductExistence(Product product){
+        boolean productExists = false;
+        for( Product prod : products){
+            if(prod.getRegistration() == product.getRegistration()){
+                productExists  = true;
+            }
+        }
+        return productExists;
+    }
+    
+    private boolean checkUserExistence(User user){
+        boolean userExists = false;
+        for( User usr : users){
+            if(usr.getRegistration() == user.getRegistration()){
+                userExists  = true;
+            }
+        }
+        return userExists;   
+    }
 }
