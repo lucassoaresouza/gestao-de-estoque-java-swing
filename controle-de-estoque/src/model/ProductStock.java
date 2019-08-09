51a7f473a0a465702e86b5795bc21a9cd3e8b8de
Lucas S <lucas.soaresouza@gmail.com>
2019-08-08 22:34:10 -0300

    Removing user from ProductStock

--- controle-de-estoque/src/model/ProductStock.java
+++ controle-de-estoque/src/model/ProductStock.java
@@ -15,11 +15,9 @@ public class ProductStock {
     
     private static ProductStock stock = null;
     private ArrayList<Product> products;
-    private ArrayList<User> users;
     
     private ProductStock(){
         this.products =  new ArrayList<>();
-        this.users =  new ArrayList<>();
     }
     
     public static ProductStock getStock(){
@@ -35,10 +33,6 @@ public class ProductStock {
         return this.products;
     }
     
-    public ArrayList<User> getUsers(){
-        return this.users;
-    }
-    
     public boolean addProduct(Product product){
         boolean productExists;        
         productExists = checkProductExistence(product);
@@ -52,21 +46,7 @@ public class ProductStock {
     public boolean removeProduct(Product product){
         return products.removeIf(prod -> (product.getRegistration() == prod.getRegistration()));
     }
-    
-    public boolean addUser(User user){
-        boolean userExists;
-        userExists = checkUserExistence(user);
-        if(!userExists){
-            return users.add(user);
-        } else {
-            return false;
-        }
-    }
-    
-    public boolean removeUser(User user){
-        return users.removeIf(usr -> (user.getRegistration() == usr.getRegistration()));
-    }
-    
+
     private boolean checkProductExistence(Product product){
         boolean productExists = false;
         for( Product prod : products){
@@ -75,15 +55,5 @@ public class ProductStock {
             }
         }
         return productExists;
-    }
-    
-    private boolean checkUserExistence(User user){
-        boolean userExists = false;
-        for( User usr : users){
-            if(usr.getRegistration() == user.getRegistration()){
-                userExists  = true;
-            }
-        }
-        return userExists;   
     }
 }
